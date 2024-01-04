93fb1dd0aedccf0f58434355d9f0ba59d633fb9b
jeatheak <thomas.heuvel+github@gmail.com>
2024-01-04 09:00:06 +0000

    add hours countdown

--- utils/christmass.py
+++ utils/christmass.py
@@ -5,13 +5,26 @@ year = 2024
 day = 5
 month = 1
 
+hour = 12
+
 def kerstlan_countdown(location='sidebar'):
     today = datetime.now()
-    kerstlan_day = datetime(year, month, day)
+    kerstlan_day = datetime(year, month, day, hour)
 
-    days_left = (kerstlan_day - today).days + 1
+    days_left = (kerstlan_day - today).days
+
+    if days_left == 0:
+        hours_left = (kerstlan_day - today).seconds / 3600
+        if hours_left < 1:
+            hours_left = 0
+        left_text = f"Hours Left: {round(hours_left)} 🎄🎅"
+
+    else:
+        if days_left < 0:
+            days_left = 0
+        left_text = f"Days Left: {days_left} 🎄🎅"
 
     if location == 'sidebar':
-        st.sidebar.header(f"Days Left: {days_left} 🎄🎅")
+        st.sidebar.header(left_text)
     else:
-        st.header(f"Days Left: {days_left} 🎄🎅")
+        st.header(left_text)
